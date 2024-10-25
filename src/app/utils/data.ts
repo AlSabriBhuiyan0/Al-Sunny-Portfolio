3896054115225c05e320b0bbee85f654940cebe9
Al Sabri Bhuiyan <65182602+AlSabriBhuiyan0@users.noreply.github.com>
2024-10-26 05:47:45 +0600

    project update

--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -43,6 +43,15 @@ export const projectsData: projectType[] = [
       'A personal portfolio website showcasing my skills, projects, and experiences as a Webdeveloper and Data Scientist. The site features a clean and modern design, making it easy for visitors to navigate and view my work.',
     Skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Next.js', 'Tailwind CSS']
   }
+  {
+    name: 'Weather App',
+    repo: 'https://github.com/AlSabriBhuiyan0/WeatherAppbyAlsunny.git',
+    url: 'https://weatherappbyalsunny-9f57ab4d7d89.herokuapp.com/',
+    image: '/Weatherappbyalsunny.png',  // Replace with the actual image path
+    description:
+      'WeatherAppbyAlsunny is a responsive web application built with React that allows users to view the weather forecast for a specific location. The app fetches weather data from the [WeatherAPI.com](https://www.weatherapi.com/) and presents it in a user-friendly interface. The app is styled using Bootstrap and custom CSS for a clean and modern design.',
+    Skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Next.js', 'Tailwind CSS']
+  }
 ]
 
 export const skillsData = [
